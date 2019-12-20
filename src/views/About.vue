7a1ed9efb953e8b623b182b20263c7b05699780f
surajSanwal <sonusanwal65@gmail.com>
2019-12-20 10:03:54 +0530

    initial commit

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,92 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div>
+    <h1>View blog posts</h1>
+    <b-table
+      show-empty
+      stacked="md"
+      :items="users"
+      :fields="fields"
+      :current-page="currentPage"
+      :per-page="perPage"
+    >
+      <template slot="name" slot-scope="row">
+        <pre> {{ row }}</pre>
+      </template>
+
+      <template slot="email" slot-scope="row">
+        <pre> {{ row }}</pre>
+      </template>
+
+      <template slot="mobile" slot-scope="row">
+        <pre> {{ row }}</pre>
+      </template>
+
+      <template slot="edit" slot-scope="row">
+        <b-button variant="warning">{{ row.value.edit }}</b-button>
+      </template>
+    </b-table>
+    <b-row>
+      <b-col md="6" class="my-1">
+        <b-pagination
+          :total-rows="totalRows"
+          :per-page="perPage"
+          v-model="currentPage"
+          class="my-0"
+        />
+      </b-col>
+    </b-row>
   </div>
 </template>
+
+<script>
+// import { mapState } from "vuex";
+export default {
+  data() {
+    return {
+      data: [],
+      users: [
+        {
+          name: "Dickerson",
+          email: "Macdonald",
+          mobile: 9760843156,
+          gender: "Male",
+          edit: "edit"
+        },
+        {
+          name: "Dickerson",
+          email: "Macdonald",
+          mobile: 9760843156,
+          gender: "Male",
+          edit: "edit"
+        },
+        {
+          name: "Dickerson",
+          email: "Macdonald",
+          mobile: 9760843156,
+          gender: "Male",
+          edit: "edit"
+        },
+        {
+          name: "Dickerson",
+          email: "Macdonald",
+          mobile: 9760843156,
+          gender: "Female",
+          edit: "edit"
+        },
+        {
+          name: "Dickerson",
+          email: "Macdonald",
+          mobile: 9760843156,
+          gender: "Male",
+          edit: "edit"
+        }
+      ],
+      currentPage: 1,
+      perPage: 5,
+      totalRows: 10,
+      pageOptions: [5, 10, 15]
+    };
+  }
+  // computed: mapState(["users"])
+};
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,112 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div>
+    <b-container class="bv-example-row">
+      <b-row>
+        <b-col></b-col>
+        <b-col
+          ><b-form @submit="onSubmit" @reset="onReset" v-if="show">
+            <b-form-group
+              id="input-group-1"
+              label="Email address:"
+              label-for="input-1"
+              description="We'll never share your email with anyone else."
+            >
+              <b-form-input
+                id="input-1"
+                v-model="form.email"
+                type="email"
+                required
+                placeholder="Enter email"
+              ></b-form-input>
+            </b-form-group>
+
+            <b-form-group
+              id="input-group-2"
+              label="Your Name:"
+              label-for="input-2"
+            >
+              <b-form-input
+                id="input-2"
+                v-model="form.name"
+                required
+                placeholder="Enter name"
+              ></b-form-input>
+            </b-form-group>
+            <b-form-group
+              id="input-group-2"
+              label="Your Mobile Number:"
+              label-for="input-3"
+              type="number"
+            >
+              <b-form-input
+                id="input-3"
+                v-model="form.mobile"
+                required
+                placeholder="Enter Mobile Number"
+              ></b-form-input>
+            </b-form-group>
+            <b-form-group
+              id="input-group-3"
+              label="Gender:"
+              label-for="input-4"
+            >
+              <b-form-select
+                id="input-4"
+                v-model="form.gender"
+                :options="gender"
+                required
+              ></b-form-select>
+            </b-form-group>
+
+            <b-button type="submit" variant="primary">Submit</b-button>
+            <b-button type="reset" variant="danger">Reset</b-button>
+          </b-form>
+          <b-card class="mt-3" header="Form Data Result">
+            <pre class="m-0">{{ form }}</pre>
+          </b-card></b-col
+        >
+        <b-col></b-col>
+      </b-row>
+    </b-container>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
-
 export default {
-  name: "home",
-  components: {
-    HelloWorld
+  data() {
+    return {
+      form: {
+        email: "",
+        name: "",
+        mobile: null,
+        gender: null
+      },
+      gender: [
+        { text: "Select Gender", value: null },
+        { text: "Male", value: "Male" },
+        { text: "Female", value: "Female" }
+      ],
+      show: true
+    };
+  },
+  methods: {
+    onSubmit(evt) {
+      evt.preventDefault();
+      alert(JSON.stringify(this.form));
+    },
+    onReset(evt) {
+      evt.preventDefault();
+      // Reset our form values
+      this.form.email = "";
+      this.form.name = "";
+      this.form.food = null;
+      this.form.checked = [];
+      // Trick to reset/clear native browser form validation state
+      this.show = false;
+      this.$nextTick(() => {
+        this.show = true;
+      });
+    }
   }
 };
 </script>
